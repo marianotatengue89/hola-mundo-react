6c6659b33414999f884aa5d88895236393b870a4
mariano_tate <mariano.tatengue@gmail.com>
2018-05-15 20:23:11 -0300

    Video 9 y 10 de react.js de CodeJobs

--- src/server/index.js
+++ src/server/index.js
@@ -5,9 +5,19 @@ import path from 'path';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 import open from 'open';
+import exphbs from 'express-handlebars';
 
 // Webpack Configuration
 import webpackConfig from '../../webpack.config.babel';
+
+// API
+import blogApi from './api/blog';
+
+//Helpers
+import * as hbsHelper from '../lib/handlebars';
+
+// Utils
+import {isMobile} from '../lib/utils/device';
 
 // Server Port
 const port = 3000;
@@ -20,6 +30,17 @@ const app = express();
 
 // Public
 app.use(express.static(path.join(__dirname, '../public')));
+
+// Handlerbars setup
+app.engine('.hbs', exphbs({
+	extbame: '.hbs', //extension del nombre
+	helpers: hbsHelper
+}));
+
+// view Engine Setup
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', '.hbs');
+
 // Webpack Compiler
 const webpackCompiler = webpack(webpackConfig);
 
@@ -28,9 +49,26 @@ if(isDevelopment){
 	app.use(webpackHotMiddleware(webpackCompiler));
 }
 
+// Device detector
+app.use((req, res, next) => {
+	res.locals.isMobile = isMobile(req.headers['user-agent']);
+
+	return next();
+});
+
+// API dispatch
+app.use('/api/blog', blogApi);
+
 // Sending all the traffic to React
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+  
+  /*Se cambia a handlerbars*/
+  //res.sendFile(path.join(__dirname, '../public/index.html'));
+
+  res.render('frontend/index', {
+  	layout: false
+  });
+
 });
 
 // Listen port 3000
